15c7154fea5db66b603e77b448b5193836562b32
Raven Pan <galtz0321@gmail.com>
2022-06-27 07:46:08 +0800

    refactor(quiz-2): 重新命名 component，並針對在 template 的 VSCode 錯誤提示給出原因。

--- quiz-2/src/components/KeyObjectValueBlock.vue
+++ quiz-2/src/components/KeyObjectValueBlock.vue
@@ -4,23 +4,21 @@ div( class="flex flex-col gap-2")
     span(class="") {{ `${keyValue.key}:` }}
     button(@click="handleToggle" class="") {{ `[${open ? '-' : '+'}]` }}
   div(v-if="open" class="pl-4")
-    template(v-for="value in keyValue.value")
-        KeyStringValue(v-if="typeof(value.value) === 'string'" :key-value="value")
-        KeyObjectValue(v-else :key-value="value")
+    template(v-for="innerKeyValue in keyValue.value")
+      //- vue 不支援在 template 中進行 Typescript 轉型，此 VSCode 提示的型別 Error 可以忽略。
+      KeyStringValueBlock(v-if="typeof(innerKeyValue.value) === 'string'" :key-value="innerKeyValue")
+      KeyObjectValueBlock(v-else :key-value="innerKeyValue")
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import KeyStringValue from './KeyStringValue.vue'
+import KeyStringValueBlock from './KeyStringValueBlock.vue'
 import type { KeyValue } from '../types/index'
 
 defineProps<{ keyValue: KeyValue }>()
 
 const open = ref(true)
-
-const handleToggle = () => {
-  open.value = !open.value
-}
+const handleToggle = () => (open.value = !open.value)
 </script>
 
 <style scoped></style>
