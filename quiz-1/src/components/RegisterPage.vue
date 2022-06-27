e3932d498eedc921db0e80095c70c035e4810f85
Raven Pan <galtz0321@gmail.com>
2022-06-27 10:41:45 +0800

    feat: login, register input become red border when error occur

--- quiz-1/src/components/RegisterPage.vue
+++ quiz-1/src/components/RegisterPage.vue
@@ -10,15 +10,45 @@ import InputVue from './input/Input.vue'
 import Button from './button/Button.vue'
 import AuthBlockVue from './layout/AuthBlock.vue'
 import { FirebaseError } from 'firebase/app'
+import { emailRegex } from '../utils/regex'
 
 const router = useRouter()
 
 const name = ref('')
 const email = ref('')
 const password = ref('')
+const nameError = ref(false)
+const emailError = ref(false)
+const passwordError = ref(false)
 const errorMessage = ref('')
 
 const register = async () => {
+  nameError.value = false
+  emailError.value = false
+  passwordError.value = false
+  errorMessage.value = ''
+
+  if (name.value === '') {
+    nameError.value = true
+    errorMessage.value = '請輸入姓名'
+    return
+  }
+  if (email.value === '') {
+    emailError.value = true
+    errorMessage.value = '請輸入 email'
+    return
+  }
+  if (!emailRegex.test(email.value)) {
+    emailError.value = true
+    errorMessage.value = '不合法的 email 格式'
+    return
+  }
+  if (password.value === '') {
+    passwordError.value = true
+    errorMessage.value = '請輸入密碼'
+    return
+  }
+
   const auth = getAuth()
 
   try {
@@ -32,6 +62,9 @@ const register = async () => {
   } catch (error) {
     if (error instanceof Error) {
       console.error(error)
+      nameError.value = true
+      emailError.value = true
+      passwordError.value = true
       errorMessage.value = error.message
       if (error instanceof FirebaseError) {
         const errorCode = error.code
@@ -46,11 +79,11 @@ const gotoLoginPage = () => router.push('/login')
 
 <template lang="pug">
 AuthBlockVue
-  h1(class="color text-lg") Register
-  InputVue(v-model="name" placeholder="name")
-  InputVue(v-model="email" placeholder="email")
-  InputVue(v-model="password" placeholder="password")
-  p(class="text-red-500") {{ errorMessage }}
+  h1(class="text-2xl font-bold") Register
+  InputVue(v-model="name" placeholder="name" :error="nameError")
+  InputVue(v-model="email" placeholder="email" :error="emailError")
+  InputVue(v-model="password" placeholder="password" type="password" :error="passwordError")
+  p(v-if="!!errorMessage" class="text-red-500 font-bold") {{ errorMessage }}
   div(class="flex flex-row gap-2 items-center justify-center")
     Button(@click="gotoLoginPage" text="Login")
     Button(@click="register" primary text="Register")
