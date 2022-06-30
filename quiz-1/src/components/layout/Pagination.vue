d7821f1d02c52479ca95e11cdc6fc80d3cb9002c
Raven Pan <galtz0321@gmail.com>
2022-06-30 14:44:54 +0800

    feat: pagination component

--- quiz-1/src/components/layout/Pagination.vue
+++ quiz-1/src/components/layout/Pagination.vue
@@ -2,85 +2,108 @@
 import { computed } from 'vue'
 
 const props = defineProps<{
-  total: number
   page: number
   pageSize: number
+  totalPage: number
 }>()
-const maxVisibleButtons = 5
-const emit = defineEmits(['pageChange'])
+const emit = defineEmits(['firstPage', 'prevPage', 'nextPage', 'lastPage'])
+const gotoFirst = () => emit('firstPage')
+const gotoLast = () => emit('lastPage')
+const gotoPrevious = () => emit('prevPage')
+const gotoNext = () => emit('nextPage')
 
-const totalPage = computed(() => Math.ceil(props.total / props.pageSize))
+const maxVisibleButtons = 3
 
 const isInFirstPage = computed(() => props.page === 1)
-const isInLastPage = computed(() => props.page === totalPage.value)
-const startPage = computed(() => {
-  if (props.page === 1) return 1
-  if (props.page === totalPage.value) totalPage.value - maxVisibleButtons + 1
-  return props.page - 1
-})
-const endPage = computed(() =>
-  Math.min(startPage.value + maxVisibleButtons - 1, totalPage.value)
+const isInLastPage = computed(() => props.page === props.totalPage)
+const showJumpToFirstPage = computed(() => props.page >= maxVisibleButtons)
+const showLeftEllipsis = computed(() => props.page >= maxVisibleButtons + 1)
+const showRightEllipsis = computed(
+  () => props.page <= props.totalPage - maxVisibleButtons
 )
-const pages = computed(() => {
+const showJumpToLastPage = computed(
+  () => props.page <= props.totalPage - (maxVisibleButtons + 1)
+)
+
+const centerPages = computed(() => {
   const range = []
-  for (let i = startPage.value; i <= endPage.value; i += 1) {
+  // prev page
+  if (props.page !== 1) {
     range.push({
-      name: i,
-      isDisabled: i === props.page,
+      name: props.page - 1,
+      isDisabled: false,
+      onclick: gotoPrevious,
     })
   }
+  // current page
+  range.push({
+    name: props.page,
+    isDisabled: true,
+  })
+  // next page
+  if (props.page !== props.totalPage) {
+    range.push({
+      name: props.page + 1,
+      isDisabled: false,
+      onclick: gotoNext,
+    })
+  }
+
   return range
 })
 
-const gotoFirst = () => emit('pageChange', 1)
-const gotoLast = () => emit('pageChange', totalPage.value)
-const gotoPrevious = () => emit('pageChange', props.page - 1)
-const gotoNext = () => emit('pageChange', props.page + 1)
-const gotoPage = (page: number) => emit('pageChange', page)
 const isPageActive = (page: number) => page === props.page
 </script>
 
 <template lang="pug">
-div(class="flex flex-row justify-center py-2 px-4")
+div(class="flex flex-row justify-center py-8 px-4")
   ul(class="list-none flex flex-row items-center gap-2")
-    li(class="inline-block px-2 py-1 border-slate-800 border-2 rounded")
+    li(class="inline-block")
+      button(
+        type="button"
+        class="flex items-center cursor-pointer"
+        @click="gotoPrevious"
+        :disabled="isInFirstPage"
+        :class="{ 'text-slate-300 cursor-not-allowed opacity-25': isInFirstPage }"
+      )
+        img(src="../../assets/chevron-left.svg" class="w-full self-center")
+    li(v-if="showJumpToFirstPage" class="inline-block px-2 py-1 border-slate-800 border-2 rounded")
       button(
         type="button"
         @click="gotoFirst"
         :disabled="isInFirstPage"
-        :class="{ 'text-slate-300': isInFirstPage }"
       ) 1
-    li(class="inline-block px-2 py-1 border-slate-800 border-2 rounded")
-      button(
-        type="button"
-        @click="gotoPrevious"
-        :disabled="isInFirstPage"
-        :class="{ 'text-slate-300': isInFirstPage }"
-      ) Previous
+    li(v-if="showLeftEllipsis" class="inline-block")
+      img(src="../../assets/ellipsis.svg" class="w-[32px] self-center")
+
     li(
+      v-for="page in centerPages"
       :key="page.name"
-      v-for="page in pages"
+      @click="page.onclick"
       class="inline-block px-2 py-1 border-slate-800 border-2 rounded"
       :class="{ 'text-white bg-slate-800': isPageActive(page.name) }")
       button(
         type="button"
-        @click="gotoPage(page.name)"
         :disabled="page.isDisabled"
       ) {{ page.name }}
-    li(class="inline-block px-2 py-1 border-slate-800 border-2 rounded")
-      button(
-        type="button"
-        @click="gotoNext"
-        :disabled="isInLastPage"
-        :class="{ 'text-slate-300': isInLastPage }"
-      ) Next
-    li(class="inline-block px-2 py-1 border-slate-800 border-2 rounded")
+
+    li(v-if="showRightEllipsis" class="inline-block")
+      img(src="../../assets/ellipsis.svg" class="w-[32px] self-center")
+    li(v-if="showJumpToLastPage" class="inline-block px-2 py-1 border-slate-800 border-2 rounded")
       button(
         type="button"
         @click="gotoLast"
         :disabled="isInLastPage"
-        :class="{ 'text-slate-300': isInLastPage }"
-      ) {{ totalPage}}
+      ) {{ totalPage }}
+    li(class="inline-block")
+      button(
+        type="button"
+        class="flex items-center cursor-pointer"
+        @click="gotoNext"
+        :disabled="isInLastPage"
+        :class="{ 'text-slate-300 cursor-not-allowed opacity-25': isInLastPage }"
+      )
+        img(src="../../assets/chevron-right.svg" class="w-full self-center")
 </template>
 
 <style scoped></style>
